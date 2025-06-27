67827625672de7d8dbff5e9f510f412f806a7157
Tu Quoc Chung <123059617+Quoc-Chung@users.noreply.github.com>
2025-06-27 14:11:05 +0700

    add font

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,13 @@ import './App.css'
 
 function App() {
   return (
-      <div></div>
+    <div>
+       <p className="bitcount text-2xl">This is Bitcount Grid Double font.</p>
+       <p className="sixtyfour text-2xl">This is Sixtyfour Convergence font.</p>
+       <p className="dancing text-2xl">This is Sixtyfour Convergence font.</p>
+     
+    </div>
+      
   );
 }
 
