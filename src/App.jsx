c20d140a5b5400edd33cce8e84be4b3d2a2b059e
Tu Quoc Chung <123059617+Quoc-Chung@users.noreply.github.com>
2025-06-27 12:56:34 +0700

    add

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,7 @@ import './App.css'
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-green-600">
-      Hello Tailwind với Vite mới!
-    </h1>
+      <div></div>
   );
 }
 
